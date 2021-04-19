24ff2f6091d2d016fa5873737364f6e38c0d89c1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-04-20 10:46:43 +1200

    added functionality to start the game

--- main.js
+++ main.js
@@ -1,54 +1,47 @@
+const btn = document.getElementById('btn');
 const score = document.getElementById('score');
 const timeLeft = document.getElementById('timeLeft');
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 
 let results = 0;
-let position ;
-let currentTime = 60
- let setTimer = null;
+let position;
+let currentTime = 60;
+let setTimer = null;
 
+btn.addEventListener('click', () => {
+  const randomSquare = () => {
+    squares.forEach(square => {
+      square.classList.remove('mole');
+    });
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+    position = randomSquare.id;
+  };
 
+  const moveMole = () => {
+    setTimer = setInterval(randomSquare, 1000);
+  };
+  moveMole();
 
-
-
-const randomSquare = () => {
-  squares.forEach(square => {
-    square.classList.remove('mole');
-  })
-  let randomSquare = squares[Math.floor(Math.random() * 9)]
-  randomSquare.classList.add('mole');
-  position = randomSquare.id;
-}
-
-
-const moveMole = () => {
-  setTimer = setInterval(randomSquare, 1000)
-}
-moveMole();
-
-
-const countDownTimer = ()=>{
-  currentTime--;
-  timeLeft.innerHTML = currentTime;
-  if (currentTime === 0) {
-    clearInterval(setCountDownTimer);
-    clearInterval(setTimer);
-    alert('GAME OVER!!Your final score is ' + results)
-  }
-
-}
-const setCountDownTimer = setInterval(countDownTimer, 1000);
-
-squares.forEach(square => {
-  square.addEventListener('mousedown', () =>{
-    if(square.id == position) {
-      results++;
-      score.innerHTML = results;
-      position = null;
-
+  const countDownTimer = () => {
+    currentTime--;
+    timeLeft.innerHTML = currentTime;
+    if (currentTime === 0) {
+      clearInterval(setCountDownTimer);
+      clearInterval(setTimer);
+      alert('GAME OVER!!Your final score is ' + results);
     }
-  })
-})
+  };
+  const setCountDownTimer = setInterval(countDownTimer, 1000);
 
-
+  squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+      if (square.id == position) {
+        results++;
+        score.innerHTML = results;
+        position = null;
+      }
+    });
+  });
+});
